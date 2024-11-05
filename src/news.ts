077cce8ef441fd2c12cafbde2f4adc1636e318d2
sr <sr@sr.me>
2024-11-05 19:04:41 +0530

    gen

--- src/news.ts
+++ src/news.ts
@@ -1,73 +1,64 @@
-import axios from 'axios';
-import { parse } from 'node-html-parser';
+import axios from "axios";
+import { parse } from "node-html-parser";
+import { writeFile, appendFile } from "fs";
 
-const cnn = "https://edition.cnn.com"
-const wp = "https://www.washingtonpost.com/"
-const gl = "https://www.bloomberg.com/asia"
-
-export interface myNews{
-    message: string;
-    url: string;
+export interface myNews {
+  message: string;
+  url: string;
 }
 
-let config = {
-    headers:{
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0"
+export let config: axios.AxiosRequestConfig = {
+  headers: {
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0",
+  },
+};
+
+export async function scrapeAndWriteNews(
+  host_url: string,
+  config: axios.AxiosRequestConfig,
+  md_file_name: string,
+  news_link_css: string,
+  news_text_css?: string
+) {
+  console.log("Into Function");
+
+  const resp = await axios.get(host_url, config);
+  const root = parse(resp.data);
+  const news_elements = root.querySelectorAll(news_link_css);
+
+  writeFile("docs/" + md_file_name + ".md", "", (err) => {
+    if (err) console.log(err);
+  });
+
+  for (let i = 0; i < news_elements.length; i++) {
+    let news_link = news_elements[i].getAttribute("href");
+    let headline: string;
+
+    if (news_text_css) {
+      headline = news_elements[i].querySelector(news_text_css)?.textContent;
+    } else {
+      headline = news_elements[i].text;
     }
-}
 
+    if (headline != undefined && headline.length > 50) {
+      let message = "[" + headline + "]";
+      let news_url: string;
 
-export async function scrapeCNN(): Promise<Array<myNews>> {
-    console.log("into cn");
-    let newsList: Array<myNews> = [];
-    const resp = await axios.get(cnn, config);
-    const root = parse(resp.data);
-    const span_list = root.getElementsByTagName("span");
-    for (let i=0; i<span_list.length; i++){
-        let headline = span_list[i].text;
-        if (headline.length > 50) {
-            const mnews: myNews = {message: "["+span_list[i].text+"]", 
-                url: "("+cnn+span_list[i].parentNode.parentNode.parentNode.getAttribute("href")!+")"};
-            newsList.push(mnews)    
-        }     
+      if (!news_link.startsWith("/")) {
+        news_url = "(" + news_link + ")";
+      } else {
+        news_url = "(" + host_url + news_link + ")";
+      }
+
+      const data = new Uint8Array(
+        Buffer.from("### " + message + news_url + "\n")
+      );
+
+      appendFile("docs/" + md_file_name + ".md", data, (err) => {
+        if (err) console.log(err);
+      });
     }
-    console.log(newsList.length);
-    return newsList;
+  }
+  console.log(news_elements.length);
 }
-
-export async function scrapeWP(): Promise<Array<myNews>> {
-    console.log("into wp");
-    let newsList: Array<myNews> = [];
-    const resp = await axios.get(wp, config);
-    const root = parse(resp.data);
-    const elem_list = root.getElementsByTagName("a");
-    for (let i=0; i<elem_list.length; i++){
-        let headline = elem_list[i].text;
-        if (headline.length > 50) {
-            const mnews: myNews = {message: "["+elem_list[i].text+"]", 
-                url: "("+elem_list[i].getAttribute("href")!+")"};
-            newsList.push(mnews)    
-        }     
-    }
-    console.log(newsList.length);
-    return newsList;
-}
-
-export async function scrapeGL(): Promise<Array<myNews>> {
-    console.log("into gl");
-    let newsList: Array<myNews> = [];
-    const resp = await axios.get(gl, config);
-    const root = parse(resp.data);
-    const span_list = root.getElementsByTagName("span");
-    for (let i=0; i<span_list.length; i++){
-        let headline = span_list[i].text;
-        if (headline.length > 50) {
-            const mnews: myNews = {message: "["+span_list[i].text+"]", 
-                url: "("+gl+span_list[i].parentNode.parentNode.parentNode.getAttribute("href")!+")"};
-            newsList.push(mnews)    
-        }     
-    }
-    console.log(newsList.length);
-    return newsList;
-}
-
--- src/scrape.ts
+++ src/scrape.ts
@@ -1,48 +1,9 @@
-import { scrapeCNN, scrapeWP, scrapeGL } from "./news";
-import { writeFile, appendFile } from 'fs';
+import { scrapeAndWriteNews, config } from "./news";
 
-async function writeCNN(){
-    // make file blank
-    writeFile('docs/World.md', "", err => {
-        if(err) console.log(err)
-    });
-    let content = await scrapeCNN();
-    for(let i=0; i<content.length; i++){
-        const data = new Uint8Array(Buffer.from("### " + content[i].message+content[i].url+"\n"));
-        appendFile('docs/World.md', data, err => {
-            if(err) console.log(err)
-        });
-    }
-}
+const cnn = "https://edition.cnn.com";
+const wp = "https://www.washingtonpost.com";
+const bl = "https://www.bloomberg.com";
 
-async function writeWP(){
-    // make file blank
-    writeFile('docs/Washington.md', "", err => {
-        if(err) console.log(err)
-    });
-    let content = await scrapeWP();
-    for(let i=0; i<content.length; i++){
-        const data = new Uint8Array(Buffer.from("### " + content[i].message+content[i].url+"\n"));
-        appendFile('docs/Washington.md', data, err => {
-            if(err) console.log(err)
-        });
-    }
-}
-
-async function writeGL(){
-    // make file blank
-    writeFile('docs/Global.md', "", err => {
-        if(err) console.log(err)
-    });
-    let content = await scrapeGL();
-    for(let i=0; i<content.length; i++){
-        const data = new Uint8Array(Buffer.from("### " + content[i].message+content[i].url+"\n"));
-        appendFile('docs/Global.md', data, err => {
-            if(err) console.log(err)
-        });
-    }
-}
-
-writeCNN();
-writeWP();
-writeGL();
+scrapeAndWriteNews(cnn, config, "CNN", "a", "span");
+scrapeAndWriteNews(wp, config, "Washington", "a");
+scrapeAndWriteNews(bl, config, "Bloomberg", "a", "span");
