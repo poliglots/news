454f8904cc805a24adfc3efe9685f36d829fb0ee
sr <sr@sr.me>
2024-11-06 17:22:29 +0530

    incog

---
+++ Makefile
@@ -0,0 +1,9 @@
+.PHONY: clean
+
+clean:
+	rm -rf docs/Bloomberg.md
+	rm -rf docs/CNN.md
+	rm -rf docs/Washington.md
+	touch docs/Bloomberg.md
+	touch docs/CNN.md
+	touch docs/Washington.md
--- src/scrape.ts
+++
@@ -1,9 +0,0 @@
-import { scrapeAndWriteNews, config } from "./news";
-
-const cnn = "https://edition.cnn.com";
-const wp = "https://www.washingtonpost.com";
-const bl = "https://www.bloomberg.com";
-
-scrapeAndWriteNews(cnn, config, "CNN", "a", "span");
-scrapeAndWriteNews(wp, config, "Washington", "a");
-scrapeAndWriteNews(bl, config, "Bloomberg", "a", "span");
